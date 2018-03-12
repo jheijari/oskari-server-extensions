476310eefc10d65054f3a56db5d03792328b3357
Sami Mäkinen <sami.makinen@nls.fi>
2018-03-12 18:23:35 +0200

    Trigger capabilities update on all layers to get supported srs info

---
+++ server-extension/src/main/java/flyway/asdi/V1_14_2__trigger_capabilities_update.java
@@ -0,0 +1,33 @@
+package flyway.asdi;
+
+import fi.nls.oskari.map.layer.OskariLayerService;
+import fi.nls.oskari.map.view.util.ViewHelper;
+import fi.nls.oskari.service.ServiceException;
+import fi.nls.oskari.service.capabilities.CapabilitiesCacheService;
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+import org.oskari.capabilities.CapabilitiesUpdateService;
+import org.oskari.service.util.ServiceFactory;
+
+import java.sql.Connection;
+import java.util.Set;
+
+/**
+ * Created by MMLDEV on 16.1.2018.
+ */
+public class V1_14_2__trigger_capabilities_update implements JdbcMigration {
+
+
+    public void migrate(Connection connection) throws Exception {
+        OskariLayerService layerService = ServiceFactory.getMapLayerService();
+
+        CapabilitiesCacheService capabilitiesCacheService = ServiceFactory.getCapabilitiesCacheService();
+        CapabilitiesUpdateService capabilitiesUpdateService = new CapabilitiesUpdateService(
+                layerService, capabilitiesCacheService);
+
+        capabilitiesUpdateService.updateCapabilities(layerService.findAll(), getSystemCRSs());
+    }
+
+    private Set<String> getSystemCRSs() throws ServiceException {
+        return ViewHelper.getSystemCRSs(ServiceFactory.getViewService());
+    }
+}
