9b6a4109599dac94ab92d85c2c54f5f61cf574b7
Jan Wolski <jan@wolski.fi>
2019-03-21 13:24:12 +0200

    Migration to add bundle

---
+++ asdi-resources/resources/flyway/asdi/V1_16_0__add_language_selector.sql
@@ -0,0 +1,7 @@
+INSERT INTO portti_bundle(name) VALUES ('language-selector');
+INSERT INTO portti_view_bundle_seq(view_id, bundle_id, seqno, bundleinstance) VALUES (
+	1,
+	(SELECT id FROM portti_bundle WHERE name = 'language-selector'),
+	(SELECT max(seqno) FROM portti_view_bundle_seq WHERE view_id = 1) + 1,
+	'language-selector'
+);
